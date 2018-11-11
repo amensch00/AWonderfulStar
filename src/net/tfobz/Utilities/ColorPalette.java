04480b7d6db5f3ea93a1b0812f676a9fb831c688
FlappyFlapp <37047812+FlappyFlapp@users.noreply.github.com>
2018-11-11 20:27:43 +0100

    Colorpalette überarbeitet

--- src/net/tfobz/Utilities/ColorPalette.java
+++ src/net/tfobz/Utilities/ColorPalette.java
@@ -9,7 +9,7 @@ public class ColorPalette {
 	public static final Color BLAU = new Color(52,152,219);
 	public static final Color VIOLETT = new Color(142,68,173);
 	
-	public static final Color INOPEN = new Color(69, 69, 69);
-	public static final Color INCLOSED = new Color(88, 14, 88);
-	public static final Color DAWE = new Color(0, 0, 0);
+	public static final Color INOPEN = new Color(236,240,241);
+	public static final Color INCLOSED = new Color(155,89,182);
+	public static final Color DAWE = new Color(44,62,80);
 }
