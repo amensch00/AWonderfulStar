a57573924cc93bbcdfcee5f5bcc471ed33016603
Elias <eliasthomaser@hotmail.com>
2018-11-14 20:20:31 +0100

    Überprufung der eingelesenen Bilder/map implementiert

--- src/net/tfobz/Daten/IMGProcessor.java
+++ src/net/tfobz/Daten/IMGProcessor.java
@@ -7,20 +7,33 @@ import java.awt.image.BufferedImage;
 import net.tfobz.BackEnd.Map;
 import net.tfobz.BackEnd.TileNode;
 import net.tfobz.BackEnd.TileType;
-import net.tfobz.Utilities.ColorPalette;
+import net.tfobz.Utilities.IllegalColorException;
 
 public class IMGProcessor {
 	private BufferedImage img;
 
+	// Farben Konstanten für das Einlesen von PNG Dateien die als Map benutzt werden können
 	private static final int RWALL = new Color(255, 0, 0).getRGB();
 	private static final int RSTREET = new Color(0, 255, 0).getRGB();
 	private static final int RSTART = new Color(0, 0, 255).getRGB();
 	private static final int RZIEL = new Color(255, 255, 0).getRGB();
 
+	/**
+	 * Wirft eine <code>IllegalArgumentException</code> wenn das Image <code>null</code> ist.
+	 * @param BufferedImage img
+	 */
 	public IMGProcessor(BufferedImage img) {
+		if (img == null)
+			throw new IllegalArgumentException();
+		
 		this.img = img;
 	}
 
+	/**
+	 * Gibt das im Konstruktor übergebene Bild als <code>Map</code> Objekt zurück
+	 * und setzt den Start und Ziel punkt in der Map
+	 * @return Bild als Map objekt
+	 */
 	public Map getMap() {
 		Map ret = new Map(img.getHeight(),img.getWidth());
 
@@ -39,6 +52,51 @@ public class IMGProcessor {
 			}
 		}
 		return ret;
+	}
+	
+	/**
+	 * Überprüft ob das übergebene Image überhapt verwendbar ist den Weg aufzulösen
+	 * @param <code>BufferedImage</code> image
+	 * @return <code>true</code> wenn das BufferedImage verwendbar ist, sonst <code>false</code>
+	 */
+	public boolean isImageValid(BufferedImage image) {
+		int startCounter = 0, zielCounter = 0;
+		
+		for (int x = 0; x < image.getWidth(); x++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				if (img.getRGB(x, y) != RSTREET || img.getRGB(x, y) != RSTART || img.getRGB(x, y) != RWALL || img.getRGB(x, y) != RZIEL)
+					throw new IllegalColorException("Bild enthält nicht verwendbare farben");
+				
+				if (image.getRGB(x, y) == RSTART)
+					startCounter++;
+				else if (image.getRGB(x, y) == RZIEL)
+					zielCounter++;
+				
+				if (startCounter > 1 || zielCounter > 1)
+					return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Überprüft ob die übergebene Map überhapt verwendbar ist den Weg aufzulösen
+	 * @param <code>Map</code> map
+	 * @return <code>true</code> wenn die map verwendbar ist, sonst <code>false</code>
+	 */
+	public boolean isMapValid(Map map) {
+		int startCounter = 0, zielCounter = 0;
+		
+		for (TileNode tn : map.getAllTNs()) {
+				if (tn.getType() == TileType.START)
+					startCounter++;
+				else if (tn.getType() == TileType.ZIEL)
+					zielCounter++;
+				
+				if (startCounter > 1 || zielCounter > 1)
+					return false;
+		}
+		return true;
 	}
 	
 }
